b167a6be3d8d7a1f677251c4e07630041eb3c3c1
xaeuun <gaeun2064@naver.com>
2024-11-22 16:08:37 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  var swiper = new Swiper(".wine", {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    slidesPerView: 4,
+    spaceBetween: 30,
+  });
+
+
+});
